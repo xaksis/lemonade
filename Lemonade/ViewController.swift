95fe7c215892de84fcab550a1982f5a9ab188400
Akshay Anand <aks9800@gmail.com>
2014-11-15 23:25:30 -0500

    Initial Commit

---
+++ Lemonade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lemonade
+//
+//  Created by Akshay Anand on 11/15/14.
+//  Copyright (c) 2014 CrayonBytes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
